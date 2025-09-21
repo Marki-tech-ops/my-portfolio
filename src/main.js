99023fd4718e0f5ef98ab361f6199c3a2377d51f
Iandexson00@gmail.com <Iandexson00@gmail.com>
2025-09-21 21:15:51 +0800

    new

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
 // import router from './router'
 
 // createApp(App).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import $ from 'jquery'
 import { createApp } from 'vue'
 import App from './App.vue'
 // import { Vue3Mq } from 'vue3-mq'
